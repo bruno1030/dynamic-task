987b3175c1a0bc1d0d210e1ef3a02bbc1bdaa325
Bruno <brunomartins1030@gmail.com>
2025-11-13 14:15:56 +0000

    Initial Dynamic Task project setup

---
+++ src/main/java/com/bruno/dynamictask/DynamictaskApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.dynamictask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamictaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamictaskApplication.class, args);
+	}
+
+}
